415450736d9ee16aa12d4a238f2dee35ae5cd6f7
Patrick Huber <prhuber@gmail.com>
2013-10-20 21:52:10 -0400

    adds more implementation to appfabric cache.

--- CacheIt.AppFabric/AppFabricCache.cs
+++ CacheIt.AppFabric/AppFabricCache.cs
@@ -81,6 +81,14 @@ namespace CacheIt.AppFabric
             }
         }
 
+        /// <summary>
+        /// Gets the specified cache entry from the cache as a <see cref="T:System.Runtime.Caching.CacheItem" /> instance.
+        /// </summary>
+        /// <param name="key">A unique identifier for the cache entry to get.</param>
+        /// <param name="regionName">Optional. A named region in the cache to which the cache was added, if regions are implemented. Because regions are not implemented in .NET Framework 4, the default is null.</param>
+        /// <returns>
+        /// The cache entry that is identified by <paramref name="key" />.
+        /// </returns>
         public override CacheItem GetCacheItem(string key, string regionName = null)
         {   
             if (regionName == null)
@@ -89,21 +97,73 @@ namespace CacheIt.AppFabric
             return cache.Get(key, regionName) as CacheItem;            
         }
 
+        /// <summary>
+        /// Gets the total number of cache entries in the cache. 
+        /// <see cref="http://stackoverflow.com/questions/4226321/appfabric-get-named-cache-object-count"/>
+        /// </summary>
+        /// <param name="regionName">Optional. A named region in the cache for which the cache entry count should be computed, if regions are implemented. The default value for the optional parameter is null.</param>
+        /// <returns>
+        /// The number of cache entries in the cache. If <paramref name="regionName" /> is not null, the count indicates the number of entries that are in the specified cache region.
+        /// </returns>
         public override long GetCount(string regionName = null)
         {
-            throw new NotImplementedException();
+            if(regionName != null)
+            {   
+                var objectsInRegion = cache.GetObjectsInRegion(regionName);
+                return objectsInRegion.Count();
+            }
+                        
+            long totalItemCount = 0;
+            foreach (string systemRegionName in cache.GetSystemRegions())
+            {
+                totalItemCount += cache.GetObjectsInRegion(regionName).Count();
+            }
+            return totalItemCount;
         }
 
+        /// <summary>
+        /// Creates an enumerator that can be used to iterate through a collection of cache entries.
+        /// </summary>
+        /// <returns>
+        /// The enumerator object that provides access to the cache entries in the cache.
+        /// </returns>
         protected override IEnumerator<KeyValuePair<string, object>> GetEnumerator()
         {
-            throw new NotImplementedException();
+            foreach (string regionName in cache.GetSystemRegions())
+            {
+                foreach (var keyValuePair in cache.GetObjectsInRegion(regionName))
+                {
+                    yield return keyValuePair;
+                }
+            }            
         }
 
+        /// <summary>
+        /// Gets a set of cache entries that correspond to the specified keys.
+        /// </summary>
+        /// <param name="keys">A collection of unique identifiers for the cache entries to get.</param>
+        /// <param name="regionName">Optional. A named region in the cache to which the cache entry or entries were added, if regions are implemented. The default value for the optional parameter is null.</param>
+        /// <returns>
+        /// A dictionary of key/value pairs that represent cache entries.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException"></exception>
         public override IDictionary<string, object> GetValues(IEnumerable<string> keys, string regionName = null)
         {
-            throw new NotImplementedException();
+            // perhaps add some code to check for object size?
+            // > 5k causes performance problems
+            return cache
+                .BulkGet(keys, regionName)
+                .ToDictionary(x=>x.Key, x=>x.Value);
         }
 
+        /// <summary>
+        /// Removes the cache entry from the cache.
+        /// </summary>
+        /// <param name="key">A unique identifier for the cache entry.</param>
+        /// <param name="regionName">Optional. A named region in the cache to which the cache entry was added, if regions are implemented. The default value for the optional parameter is null.</param>
+        /// <returns>
+        /// An object that represents the value of the removed cache entry that was specified by the key, or null if the specified entry was not found.
+        /// </returns>
         public override object Remove(string key, string regionName = null)
         {
             var instance = Get(key, regionName);
@@ -114,14 +174,18 @@ namespace CacheIt.AppFabric
             return instance;
         }
 
+        /// <summary>
+        /// Inserts the cache entry into the cache as a <see cref="T:System.Runtime.Caching.CacheItem" /> instance, specifying information about how the entry will be evicted.
+        /// </summary>
+        /// <param name="item">The cache item to add.</param>
+        /// <param name="policy">An object that contains eviction details for the cache entry. This object provides more options for eviction than a simple absolute expiration.</param>
         public override void Set(CacheItem item, CacheItemPolicy policy)
         {
             // need to convert policy to TimeSpan
             if (item.RegionName == null)
                 cache.Put(item.Key, item, DateTime.Now - policy.AbsoluteExpiration.DateTime);
             else
-                cache.Put(item.Key, item, DateTime.Now - policy.AbsoluteExpiration.DateTime, item.RegionName);
-            throw new NotImplementedException();
+                cache.Put(item.Key, item, DateTime.Now - policy.AbsoluteExpiration.DateTime, item.RegionName);            
         }
     }
 }
