97ac9ab37a4ca54e15248c1f01fddcd1a1a7b5c8
Patrick Huber <prhuber@gmail.com>
2013-10-22 20:24:51 -0400

    adds appfabric startup script.

---
+++ CacheIt.IntegrationTests/StartAppFabric.ps1
@@ -0,0 +1,4 @@
+﻿set COMPLUS_ApplicationMigrationRuntimeActivationConfigPath="c:\Program Files\AppFabric 1.1 for Windows Server"
+Import-Module DistributedCacheAdministration
+Use-CacheCluster
+Start-CacheCluster
