ad6c95239d61e8332105328310c8fd559b3f07df
Patrick Huber <prhuber@gmail.com>
2013-10-29 23:44:53 -0400

    updates index and repository for testing

---
+++ CacheIt.Lucene.UnitTests/SimpleDataIndex.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Version = Lucene.Net.Util.Version;
+
+namespace CacheIt.Lucene.UnitTests
+{
+    public class SimpleDataIndex
+    {        
+        private Directory directory;
+        
+        public SimpleDataIndex(List<SimpleData> items, Directory directory)
+        {
+            this.directory = directory;
+            AddUpdateLuceneIndex(items);
+        }
+
+        public IEnumerable<SimpleData> Search(string searchQuery)
+        {
+            // validation
+            if (string.IsNullOrEmpty(
+                searchQuery
+                    .Replace("*", "")
+                    .Replace("?", "")))
+                return new List<SimpleData>();
+
+            using (var searcher = new IndexSearcher(directory, false))
+            {
+                var analyzer = new StandardAnalyzer(Version.LUCENE_30);
+                var parser = new MultiFieldQueryParser(Version.LUCENE_30, new[] { "Id", "Name", "Description" }, analyzer);
+                var query = parseQuery(searchQuery, parser);
+                var hits = searcher.Search(query, null, 1000, Sort.RELEVANCE).ScoreDocs;
+                var results = Map(hits, searcher);
+                analyzer.Close();
+                searcher.Dispose();
+                return results;
+            }
+        }
+
+        /// <summary>
+        /// Maps the specified document.
+        /// </summary>
+        /// <param name="document">The document.</param>
+        /// <returns></returns>
+        private SimpleData Map(Document document)
+        {
+            return new SimpleData
+            {
+                Id = Convert.ToInt32(document.Get("Id")),
+                Name = document.Get("Name"),
+                Description = document.Get("Description")
+            };
+        }
+
+        private IEnumerable<SimpleData> Map(IEnumerable<Document> hits)
+        {
+            return hits.Select(Map).ToList();
+        }
+
+        private IEnumerable<SimpleData> Map(IEnumerable<ScoreDoc> hits, IndexSearcher searcher)
+        {
+            return hits.Select(hit => Map(searcher.Doc(hit.Doc))).ToList();
+        }
+
+        private static Query parseQuery(string searchQuery, QueryParser parser)
+        {
+            Query query;
+            try
+            {
+                query = parser.Parse(searchQuery.Trim());
+            }
+            catch (ParseException)
+            {
+                query = parser.Parse(QueryParser.Escape(searchQuery.Trim()));
+            }
+            return query;
+        } 
+
+        private void AddUpdateLuceneIndex(IEnumerable<SimpleData> simpleDataList)
+        {
+            using (var analyzer = new StandardAnalyzer(Version.LUCENE_30))
+            {
+                using (var writer = new IndexWriter(directory, analyzer, IndexWriter.MaxFieldLength.UNLIMITED))
+                {
+                    foreach (var simpleData in simpleDataList)
+                    {
+                        AddToLuceneIndex(simpleData, writer);
+                    }
+                }
+            }
+        }
+
+        private void AddToLuceneIndex(SimpleData simpleData, IndexWriter writer)
+        {
+            // remove older index entries
+            var searchQuery = new TermQuery(new Term("Id", simpleData.Id.ToString()));
+            writer.DeleteDocuments(searchQuery);
+
+            // add new index entry
+            var doc = new Document();
+
+            // add lucene fields mapped to our object
+            doc.Add(new Field("Id", simpleData.Id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            doc.Add(new Field("Name", simpleData.Name, Field.Store.YES, Field.Index.ANALYZED));
+            doc.Add(new Field("Description", simpleData.Description, Field.Store.YES, Field.Index.ANALYZED));
+
+            // add entry to the index
+            writer.AddDocument(doc);
+        }
+    }
+}
--- CacheIt.Lucene.UnitTests/SimpleDataRepository.cs
+++ CacheIt.Lucene.UnitTests/SimpleDataRepository.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Lucene.Net.Search;
-using Lucene.Net.Index;
-using Lucene.Net.Documents;
-using Lucene.Net.Analysis.Standard;
-using Version = Lucene.Net.Util.Version;
-using Lucene.Net.Store;
 using System.Runtime.Caching;
+using Lucene.Net.Store;
+using CacheIt.Lucene.Store;
+using Lucene.Net.Documents;
+using System;
+using Lucene.Net.Search;
 
 namespace CacheIt.Lucene.UnitTests
 {
@@ -23,47 +20,48 @@ namespace CacheIt.Lucene.UnitTests
             new SimpleData {Id = 3, Name = "Chicago", Description = "City in USA"},
             new SimpleData {Id = 4, Name = "Mumbai", Description = "City in India"},
             new SimpleData {Id = 5, Name = "Hong-Kong", Description = "City in Hong-Kong"},
-        };     
-  
+        };
+
+        private static SimpleDataIndex simpleDataIndex;
+
+        static SimpleDataRepository()
+        { 
+            var objectCache = new MemoryCache("Default");
+            var cacheDirectory = new CacheDirectory(objectCache, "/path/to/directory");
+            simpleDataIndex = new SimpleDataIndex(Items, cacheDirectory);
+        }
+
+        public SimpleDataRepository()
+        {
+        }
+
+        /// <summary>
+        /// Gets the specified unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier.</param>
+        /// <returns></returns>
         public SimpleData Get(int id)
         {
             return Items.FirstOrDefault(x => x.Id.Equals(id));
         }
 
-        public List<SimpleData> GetAll()
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<SimpleData> GetAll()
         {
             return Items;        
         }
 
-        static SimpleDataRepository()
+        /// <summary>
+        /// Searches the specified query.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <returns></returns>
+        public IEnumerable<SimpleData> Search(string query)
         {
-            var objectCache = new MemoryCache("Default");
-            var directory = new CacheIt.Lucene.Store.CacheDirectory(objectCache, "/path/to/directory");
-
-        }
-
-        private static void AddUpdateLuceneIndex(IEnumerable<SimpleData> simpleDataList)
-        { 
-            var analyzer = new StandardAnalyzer(Version.LUCENE_30);
-            using(var writer = new IndexWriter())
-        }
-
-        private static void AddToLuceneIndex(SimpleData simpleData, IndexWriter writer)
-        {
-            // remove older index entries
-            var searchQuery = new TermQuery(new Term("Id", simpleData.Id.ToString()));
-            writer.DeleteDocuments(searchQuery);
-
-            // add new index entry
-            var doc = new Document();
-
-            // add lucene fields mapped to our object
-            doc.Add(new Field("Id", simpleData.Id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-            doc.Add(new Field("Name", simpleData.Name, Field.Store.YES, Field.Index.ANALYZED));
-            doc.Add(new Field("Description", simpleData.Description, Field.Store.YES, Field.Index.ANALYZED));
-
-            // add entry to the index
-            writer.AddDocument(doc);
-        }
+            return simpleDataIndex.Search(query);
+        }       
     }
 }
