56645d9eacc441697d38c2f171624a579abc393c
Patrick Huber <prhuber@gmail.com>
2013-11-08 08:04:14 -0500

    adds stream factory interface

---
+++ CacheIt/IO/IStreamFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CacheIt.IO
+{
+    /// <summary>
+    /// Factory interface for creating streams. Should help with dependency on a particular stream class.
+    /// </summary>
+    public interface IStreamFactory
+    {
+        /// <summary>
+        /// Creates a stream
+        /// </summary>
+        /// <returns>the stream that was created.</returns>
+        Stream Create();
+    }
+}
