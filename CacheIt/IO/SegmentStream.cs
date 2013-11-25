4e1d508df0161061063323d0ab3f2228fd7980c4
Patrick Huber <prhuber@gmail.com>
2013-11-25 10:23:36 -0500

    updates comments.

--- CacheIt/IO/SegmentStream.cs
+++ CacheIt/IO/SegmentStream.cs
@@ -7,50 +7,91 @@ using System.Text;
 
 namespace CacheIt.IO
 {
+    /// <summary>
+    /// A segment stream will use chunking to send data to the cache. 
+    /// </summary>
     public class SegmentStream : Stream
     {
+        /// <summary>
+        /// The buffered stream used to prevent thrashing of the source cache.
+        /// </summary>
         private BufferedStream bufferedStream;
-
 
         public const int DefaultSegmentSize = 1024;
         public const string DefaultRegion = null;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SegmentStream"/> class.
+        /// </summary>
+        /// <param name="objectCache">The object cache.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="region">The region.</param>
         public SegmentStream(ObjectCache objectCache, string key, string region = DefaultRegion)
             : this(objectCache, key, DefaultSegmentSize)
         { 
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SegmentStream"/> class.
+        /// </summary>
+        /// <param name="objectCache">The object cache.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="segmentSize">Size of the segment.</param>
+        /// <param name="region">The region.</param>
         public SegmentStream(ObjectCache objectCache, string key, int segmentSize, string region = DefaultRegion)
         {
             bufferedStream = new BufferedStream(
                 new InternalSegmentStream(objectCache, key, segmentSize, region));
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the current stream supports reading.
+        /// </summary>
+        /// <returns>true if the stream supports reading; otherwise, false.</returns>
         public override bool CanRead
         {
             get { return bufferedStream.CanRead; }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the current stream supports seeking.
+        /// </summary>
+        /// <returns>true if the stream supports seeking; otherwise, false.</returns>
         public override bool CanSeek
         {
             get { return bufferedStream.CanSeek; }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the current stream supports writing.
+        /// </summary>
+        /// <returns>true if the stream supports writing; otherwise, false.</returns>
         public override bool CanWrite
         {
             get { return bufferedStream.CanWrite; }
         }
 
+        /// <summary>
+        /// Clears all buffers for this stream and causes any buffered data to be written to the underlying device.
+        /// </summary>
         public override void Flush()
         {
             bufferedStream.Flush();
         }
 
+        /// <summary>
+        /// Gets the length in bytes of the stream.
+        /// </summary>
+        /// <returns>A long value representing the length of the stream in bytes.</returns>
         public override long Length
         {
             get { return bufferedStream.Length; }
         }
 
+        /// <summary>
+        /// Gets or sets the position within the current stream.
+        /// </summary>
+        /// <returns>The current position within the stream.</returns>
         public override long Position
         {
             get
@@ -63,21 +104,48 @@ namespace CacheIt.IO
             }
         }
 
+        /// <summary>
+        /// Reads a sequence of bytes from the current stream and advances the position within the stream by the number of bytes read.
+        /// </summary>
+        /// <param name="buffer">An array of bytes. When this method returns, the buffer contains the specified byte array with the values between <paramref name="offset" /> and (<paramref name="offset" /> + <paramref name="count" /> - 1) replaced by the bytes read from the current source.</param>
+        /// <param name="offset">The zero-based byte offset in <paramref name="buffer" /> at which to begin storing the data read from the current stream.</param>
+        /// <param name="count">The maximum number of bytes to be read from the current stream.</param>
+        /// <returns>
+        /// The total number of bytes read into the buffer. This can be less than the number of bytes requested if that many bytes are not currently available, or zero (0) if the end of the stream has been reached.
+        /// </returns>
         public override int Read(byte[] buffer, int offset, int count)
         {
             return bufferedStream.Read(buffer, offset, count);
         }
 
+        /// <summary>
+        /// Sets the position within the current stream.
+        /// </summary>
+        /// <param name="offset">A byte offset relative to the <paramref name="origin" /> parameter.</param>
+        /// <param name="origin">A value of type <see cref="T:System.IO.SeekOrigin" /> indicating the reference point used to obtain the new position.</param>
+        /// <returns>
+        /// The new position within the current stream.
+        /// </returns>
         public override long Seek(long offset, SeekOrigin origin)
         {
             return bufferedStream.Seek(offset, origin);
         }
 
+        /// <summary>
+        /// Sets the length of the current stream.
+        /// </summary>
+        /// <param name="value">The desired length of the current stream in bytes.</param>
         public override void SetLength(long value)
         {
             bufferedStream.SetLength(value);
         }
 
+        /// <summary>
+        /// Writes a sequence of bytes to the current stream and advances the current position within this stream by the number of bytes written.
+        /// </summary>
+        /// <param name="buffer">An array of bytes. This method copies <paramref name="count" /> bytes from <paramref name="buffer" /> to the current stream.</param>
+        /// <param name="offset">The zero-based byte offset in <paramref name="buffer" /> at which to begin copying bytes to the current stream.</param>
+        /// <param name="count">The number of bytes to be written to the current stream.</param>
         public override void Write(byte[] buffer, int offset, int count)
         {
             bufferedStream.Write(buffer, offset, count);
