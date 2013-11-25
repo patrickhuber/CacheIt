1243299d851eab802c840bd7fff389799da1a5e5
Patrick Huber <prhuber@gmail.com>
2013-11-25 09:06:04 -0500

    adds extensions for Wacel

---
+++ CacheIt.Wacel/Cache/ObjectCacheDataProvider.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Ted.Wacel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CacheIt.Wacel.Cache
+{
+    public class ObjectCacheDataProvider : IDataProvider
+    {
+        public IEnumerable<KeyValuePair<string, object>> BulkGet(List<string> keys, string partition = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BulkPut(IEnumerable<KeyValuePair<string, object>> values, string partition = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataProvider Clone(string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataProviderConfiguration Configuration
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Delete(string key, string partition = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Get(string key, string partition = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, object>> GetAll(string partition = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Put(string key, object value, string partition = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string key, object value, string partition = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CacheIt.Wacel/Cache/ObjectCacheLockProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Ted.Wacel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CacheIt.Wacel.Cache
+{
+    public class ObjectCacheLockProvider : ILockProvider
+    {
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Increment(string key, long increment, long defaultValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PutAndUnlock(string key, long value, object lockHandle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PutInSub(string key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long ReadAndLock(string key, long defaultValue, TimeSpan timespan, out object lockHandle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unlock(string key, object lockHandle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
