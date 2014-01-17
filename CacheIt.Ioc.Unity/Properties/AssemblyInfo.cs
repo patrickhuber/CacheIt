2d4e037b7df7f82e54d6a3e0099cca2fdd399888
Patrick Huber <prhuber@gmail.com>
2014-01-16 21:52:13 -0500

    renames unity project

--- CacheIt.Ioc.Unity/Properties/AssemblyInfo.cs
+++ CacheIt.Ioc.Unity/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CacheIt.Ioc.Unity")]
+[assembly: AssemblyTitle("CacheIt.Unity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CacheIt.Ioc.Unity")]
+[assembly: AssemblyProduct("CacheIt.Unity")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
