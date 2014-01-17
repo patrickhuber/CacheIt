9c1826eef12f52437f1d471d5824970decb70e8c
Patrick Huber <prhuber@gmail.com>
2014-01-16 21:37:02 -0500

    adds reddis

--- CacheIt.Redis/Properties/AssemblyInfo.cs
+++ CacheIt.Redis/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CacheIt.Redis")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bb1d408b-db33-48ac-a174-1399b94497c0")]
+[assembly: Guid("923f99ce-2bfc-47a1-87f6-29bfdc9e4e50")]
 
 // Version information for an assembly consists of the following four values:
 //
