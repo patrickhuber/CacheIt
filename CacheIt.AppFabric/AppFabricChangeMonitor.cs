c44018e279bc53da7051e548f0313b31328d152b
Patrick Huber <prhuber@gmail.com>
2014-01-17 04:36:52 -0500

    adds inintial notification descriptor

--- CacheIt.AppFabric/AppFabricChangeMonitor.cs
+++ CacheIt.AppFabric/AppFabricChangeMonitor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ApplicationServer.Caching;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -7,14 +8,50 @@ using System.Text;
 namespace CacheIt.AppFabric
 {
     /// <summary>
-    /// 
+    /// <see cref="http://msdn.microsoft.com/en-us/library/ee790841(v=azure.10).aspx"/>
     /// </summary>
-    public sealed class AppFabricChangeMonitor : CacheEntryChangeMonitorBase
+    public sealed class AppFabricChangeMonitor : CacheEntryChangeMonitorBase, IDisposable
     {
-        public AppFabricChangeMonitor(ReadOnlyCollection<string> keys, string regionName = null)
+        private DataCache dataCache;
+        private IList<DataCacheNotificationDescriptor> notificationDescriptorList;
+
+        public AppFabricChangeMonitor(ReadOnlyCollection<string> keys, string regionName, DataCache dataCache)
             : base(keys, regionName) 
-        { 
-            
-        }     
+        {
+            this.dataCache = dataCache;
+            this.notificationDescriptorList = new List<DataCacheNotificationDescriptor>();
+
+            foreach (var key in keys)
+            {
+                var notificationDescriptor = 
+                    this.dataCache.AddItemLevelCallback(
+                        key, 
+                        DataCacheOperations.RemoveItem 
+                            | DataCacheOperations.ReplaceItem 
+                            | DataCacheOperations.AddItem, 
+                        ItemChangedCallbackDelegate);
+                notificationDescriptorList.Add(notificationDescriptor);
+            }
+        }
+
+        private void ItemChangedCallbackDelegate(
+            string cacheName, 
+            string region, 
+            string key, 
+            DataCacheItemVersion version, 
+            DataCacheOperations operation, 
+            DataCacheNotificationDescriptor descriptior)
+        {
+            if(cacheKeys.Contains(key) && region == regionName)
+                this.OnChanged(null);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposing || notificationDescriptorList.Count == 0)
+                return;
+            foreach (var notificationDescriptor in notificationDescriptorList)
+                dataCache.RemoveCallback(notificationDescriptor);
+        }
     }
 }
