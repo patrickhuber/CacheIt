68516eda7c069b709b83ab91bce428f77894a24b
Patrick Huber <prhuber@gmail.com>
2014-01-16 21:50:19 -0500

    adds unity projects

---
+++ CacheIt.Ioc.Unity/UnityDepedencyResolver.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CacheIt.Ioc.Unity
+{
+    /// <summary>
+    /// Creates a Unity IDependencyResolver
+    /// </summary>
+    public class UnityDepedencyResolver : IDependencyResolver
+    {
+        private IUnityContainer container;
+
+        public UnityDepedencyResolver(IUnityContainer container)
+        {
+            this.container = container;
+        }
+
+        public T GetService<T>()
+        {
+            return container.Resolve<T>();
+        }
+    }
+}
