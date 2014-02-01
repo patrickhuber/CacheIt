58dcf6a9c07ed8dff4ad363dc3bcb4088e1969c2
Patrick Huber <prhuber@gmail.com>
2014-02-01 13:50:11 -0500

    adds stub session state store provider

---
+++ CacheIt.Web/ObjectCacheSessionStateStoreProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Web.SessionState;
+
+namespace CacheIt.Web
+{
+    public class ObjectCacheSessionStateStoreProvider : SessionStateStoreProviderBase 
+    {
+        public ObjectCacheSessionStateStoreProvider(ObjectCache objectCache)
+        { }
+
+        public override SessionStateStoreData CreateNewStoreData(System.Web.HttpContext context, int timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateUninitializedItem(System.Web.HttpContext context, string id, int timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void EndRequest(System.Web.HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override SessionStateStoreData GetItem(System.Web.HttpContext context, string id, out bool locked, out TimeSpan lockAge, out object lockId, out SessionStateActions actions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override SessionStateStoreData GetItemExclusive(System.Web.HttpContext context, string id, out bool locked, out TimeSpan lockAge, out object lockId, out SessionStateActions actions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InitializeRequest(System.Web.HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReleaseItemExclusive(System.Web.HttpContext context, string id, object lockId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveItem(System.Web.HttpContext context, string id, object lockId, SessionStateStoreData item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ResetItemTimeout(System.Web.HttpContext context, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetAndReleaseItemExclusive(System.Web.HttpContext context, string id, SessionStateStoreData item, object lockId, bool newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool SetItemExpireCallback(SessionStateItemExpireCallback expireCallback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
