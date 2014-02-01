ae5bf60f0e4c7675d2c308056a966de1c9701d8b
Patrick Huber <prhuber@gmail.com>
2014-02-01 13:56:42 -0500

    adds composition dependency resolver

---
+++ CacheIt/Ioc/CompositionDependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+
+namespace CacheIt.Ioc
+{
+    public class CompositionDependencyResolver : IDependencyResolver
+    {
+        private CompositionContainer compositionContainer;
+
+        public CompositionDependencyResolver(CompositionContainer container)
+        {
+            this.compositionContainer = container;
+        }
+
+        public void Register<T>(T value)
+        {
+            this.compositionContainer.ComposeExportedValue<T>(value);
+        }
+
+        public void Register<T>(T value, string name)
+        {
+            this.compositionContainer.ComposeExportedValue<T>(name, value);
+        }
+
+        public T GetService<T>()
+        {
+            return compositionContainer.GetExportedValue<T>();
+        }
+
+        public T GetService<T>(string name)
+        {
+            return compositionContainer.GetExportedValue<T>(name);
+        }
+    }
+}
+
