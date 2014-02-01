9921cde6f9d516f570fc3e5e536acb234b08d2bd
Patrick Huber <prhuber@gmail.com>
2014-02-01 13:46:12 -0500

    adds stubbed wacel support

--- CacheIt.Wacel/Cache/ObjectCacheDataProvider.cs
+++ CacheIt.Wacel/Cache/ObjectCacheDataProvider.cs
@@ -2,67 +2,77 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Text;
 
 namespace CacheIt.Wacel.Cache
 {
     public class ObjectCacheDataProvider : IDataProvider
     {
+        private ObjectCache objectCache;
+        private DataProviderConfiguration configuration;
+        private string region;
+
+        public ObjectCacheDataProvider(ObjectCache objectCache, string region = null)
+        {
+            this.objectCache = objectCache;
+            this.region = region;
+        }
+
         public IEnumerable<KeyValuePair<string, object>> BulkGet(List<string> keys, string partition = null)
         {
-            throw new NotImplementedException();
+            return objectCache.GetValues(keys, region);
         }
 
         public void BulkPut(IEnumerable<KeyValuePair<string, object>> values, string partition = null)
         {
-            throw new NotImplementedException();
+            foreach (var keyValuePair in values)
+                objectCache.Set(keyValuePair.Key, keyValuePair.Value, ObjectCache.InfiniteAbsoluteExpiration, region);
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            objectCache.Clear(region);            
         }
 
         public IDataProvider Clone(string newName)
         {
-            throw new NotImplementedException();
+            return (IDataProvider)new ObjectCacheDataProvider(this.objectCache, newName)
+            {
+                Configuration = (DataProviderConfiguration)this.configuration.Clone()
+            };
         }
 
         public DataProviderConfiguration Configuration
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get { return this.configuration; }
+            set { this.configuration = value; }
         }
 
         public void Delete(string key, string partition = null)
         {
-            throw new NotImplementedException();
+            objectCache.Remove(key, region);
         }
 
         public object Get(string key, string partition = null)
         {
-            throw new NotImplementedException();
+            return objectCache.Get(key, region);
         }
 
         public IEnumerable<KeyValuePair<string, object>> GetAll(string partition = null)
         {
-            throw new NotImplementedException();
+            foreach (var keyValuePair in objectCache)
+                yield return keyValuePair;
         }
 
         public void Put(string key, object value, string partition = null)
         {
-            throw new NotImplementedException();
+            objectCache.Add(key, value, ObjectCache.InfiniteAbsoluteExpiration, region);
         }
 
         public void Update(string key, object value, string partition = null)
         {
-            throw new NotImplementedException();
+            objectCache.Set(key, value, region);
         }
     }
 }
